94ece4734a9e45c43cb229f9af1e2a9668730568
wayne <wayne.fu@jfz.com>
2020-01-15 12:15:39 +0800

    spring boot fatjar

---
+++ springboot-fatjar/src/main/java/com/flydean/FatJarApp.java
@@ -0,0 +1,15 @@
+package com.flydean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wayne
+ * @version FatJarApp,  2020/1/15 11:06 上午
+ */
+@SpringBootApplication
+public class FatJarApp {
+    public static void main(String[] args) {
+        SpringApplication.run(FatJarApp.class, args);
+    }
+}
