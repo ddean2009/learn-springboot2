7a297554ba6cfc5a771116965dec1499ff7265d2
wayne <wayne.fu@jfz.com>
2020-02-21 23:25:21 +0800

    spring boot h2 database

---
+++ springboot-h2/src/main/java/com/flydean/H2App.java
@@ -0,0 +1,16 @@
+package com.flydean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wayne
+ * @version H2App,  2020/2/21 10:53 下午
+ */
+@SpringBootApplication
+public class H2App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(H2App.class, args);
+    }
+}
